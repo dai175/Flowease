d36f72f40458201dacfb72428be39a1b5633a2a4
Daisuke Oba <dai175dai@gmail.com>
2025-12-30 12:46:19 +0900

    Initial Commit

---
+++ Flowease/FloweaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloweaseApp.swift
+//  Flowease
+//
+//  Created by Daisuke Ooba on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct FloweaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
